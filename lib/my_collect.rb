9243867e779386309c8ef6e6a7c69389a9feea91
authorbeard <msnolan@gmail.com>
2015-11-23 17:51:07 -0500

    built my_collect

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  
+  i = 0 
+  first_names = []
+  while i < array.length
+    first_names << yield(array[i].split(" ").first)
+    i += 1
+  end
+  first_names 
+
+
+
+end
 
 
