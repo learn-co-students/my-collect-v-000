4b947444453bf9fd8726828858a9514fb3d5066a
Karuna <imkaruna@gmail.com>
2015-12-01 14:54:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(languages)
+  i=0
+  collection = []
+  length = languages.length
+  while length > 0 
+    collection << yield(languages[i])
+    i+=1
+    length -=1
+  end 
+  return collection
+end
