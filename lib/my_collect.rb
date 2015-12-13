4df1c0df2a6157e505bd770510c4cbbec52fb828
Lukeghenco <lukeghenco@gmail.com>
2015-12-13 12:25:16 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+#my_collect
+def my_collect(array)
+  i = 0
+  first_names = []
+  while i < array.length
+    first_names << yield(array[i])
+    i += 1
+  end
+  first_names
+end
 
+# call #my_collect
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+
+# => ["Tim", "Tom", "Jim"]
 
