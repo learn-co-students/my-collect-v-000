0eb6361838d95bf0db3cb76980e5c37da7862ec3
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-12-15 02:33:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(array)
+i=0
+  new_array=[]
 
+  if array.length==0
+    puts "empty array"
+  elsif array[0].include? (' ')
+    while i<array.length
+    a=array[i][0]
+    new_array << yield(a)
+    i+=1
+  end
+  else
+    while i<array.length
+    a=array[i]
+    new_array << yield(a)
+    i+=1
+  end
+  end
+    return new_array
 
+end
