89c44d28ae1f9a2ad8f4e8b3314005606db0b967
phamducv <phamducv@gmail.com>
2015-12-27 01:45:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,8 @@ def my_collect(array)
         collect=[]
         while i < array.length
                 collect << yield(array[i])
-                i+1
+                i+=1
         end
         collect
 end
+
