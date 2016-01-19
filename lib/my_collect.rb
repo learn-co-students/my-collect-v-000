69bb07f4b43faa4fa625e5743bafec38f5fa3262
lcorr8 <l.corr.v@gmail.com>
2016-01-18 19:58:47 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(students)
+  i = 0
+  names = []
 
+  while  i < students.length
+     names << yield(students[i])
+    i += 1
+
+  end
+  names 
+end
