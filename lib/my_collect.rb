e4da3892cc701e9b7ffc012a8c792d39432ef54c
smithWEBtek <brad@smithwebtek.com>
2016-01-19 21:44:59 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+ 
+def my_collect(array)
+  i = 0
+  fnames = []
+  while i < array.length
+    fnames << yield(array[i])
+    i = i+1
+    end
+  return fnames
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |item| item.split(" ").first }
