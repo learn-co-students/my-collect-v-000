2d6db365d070f0c080965a82692fa58ddea10192
mkrisch <mike.krisch@gmail.com>
2016-02-01 07:12:40 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+  i = 0
+  names = []
+  while i < array.length
+    names << yield(array[i])
+    i += 1
+  end
+  names
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name| name.split(" ").first }
