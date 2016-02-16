c5e7f0296d6ea6a09762ddb09009e537d747a6f7
jsbenning <jbenninghoff1970@yahoo.com>
2016-02-16 20:45:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  if array.length == 0
+    print "Empty array!"
+  else
+    new_collect = []
+    count = 0
+    while count < array.length
+      new_collect << yield(array[count])
+      count += 1
+    end
+    new_collect
+  end
+end
 
