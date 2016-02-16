51adb7ce6f33925115009e6aaa12ab441074a10a
johnsontaiwo <josseytee@gmail.com>
2016-02-16 01:46:01 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect (languages)
+  i = 0
+  new_array = []
+  while i < languages.length
+   new_array << yield(languages[i])
+   i += 1
+ end
+ new_array
+end
+ 
+ new_words = ["one", "two", "three", "four"]
+  my_collect([new_words]) do |word|
+   word
+ end
 
 
