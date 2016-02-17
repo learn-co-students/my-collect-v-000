84c0d569f24c092263ac741fccf85dade8e6dfe9
the-widget <brennenawana108ny@gmail.com>
2016-02-16 21:32:26 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(students)
+  if block_given?
+    i = 0
+    collector = []
+    while i < students.length
+      collector << yield(students[i])
+      i += 1
+    end
+    collector
+  else
+    return "Not a block!"
+  end
+end
 
