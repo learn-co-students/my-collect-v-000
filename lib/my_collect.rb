8c5da7a4f60c48fd9d63159c237a80b6c24635d6
jchu4483 <jchu4483@gmail.com>
2016-03-09 19:44:52 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(students)
+  first_names = []
+  i = 0
+  while students.length > i
+    var = yield(students[i]) 
+    first_names << var
+    i += 1
+  end
+  first_names
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|names| names.split(" ").first}
