9f01335f80ae84570b7056a07ad84e2f6c1e9752
phaltoe <reverse9@live.com>
2016-03-11 14:06:51 -0300

    done
    done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+def my_collect(languages)
+  upcased_languages = []
+  languages.each do |language|
+    upcased_languages << yield(language)
+  end
+  upcased_languages
+end
 
-
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |x| x.upcase }
