3e761006d1bb42570a109d4f3e532e5e6ae253f2
808dward <808dward@gmail.com>
2016-03-14 23:20:30 -0400

    iterated.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+	i = 0
+	collect = []
+	while i < array.length
+		collection << yield(array[i])
+		i += 1
+	end
+	collect
+end
 
-
+# do |name|
+#  name.split(" ").first
+#end
