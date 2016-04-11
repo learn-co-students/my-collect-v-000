9b5bd2d555811b34a5542b170b7ed8238acac545
tmb41 <tylor.balson@gmail.com>
2016-04-11 12:47:04 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
 
+
+def my_collect(students)
+  new_students_list = []
+  counter = 0
+  while counter < students.length
+    new_students_list << yield(students[counter])
+    counter = counter + 1
+  end
+  new_students_list
+
+end
+
