b4be4fed4eaf8892bf32ddd26604794209a50795
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2016-04-20 17:32:05 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(languages)
+  if languages.empty?
+  	puts "array empty"
+  else 
+  	i = 0
+  	new_array = []
+  	while i < languages.length
+  	new_array << yield(languages[i])
+  	i += 1
+  end
+    new_array 
+  end
+end
+
+
+
+
+
 
 
