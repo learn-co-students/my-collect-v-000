2103b5a0058a57c502bd9c927812b440649d0dad
beingy <beingy@gmail.com>
2016-05-03 12:38:29 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(students)
+	counter = 0
+	first_names = []
+	while counter < students.length do
+		first_names[counter] = yield(students[counter])
+		counter = counter + 1
+	end
+	first_names
+end
