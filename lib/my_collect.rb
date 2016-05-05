efbacbc05e1ce89ae6b063249e7a86a76cdbda1f
iandevivi <iandevivi@gmail.com>
2016-05-05 13:19:10 -0400

    completed collect lab using yield

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  return_array = []
+  if block_given?
+    array.each do |e|
+      return_array << yield(e)
+    end
+  else
+    "No block given."
+  end
+  return_array
+end
