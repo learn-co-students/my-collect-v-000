7a8eded448833d807d2a742728795c9a916f0925
steveafrost <ffrostt@gmail.com>
2016-05-06 23:10:58 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
-
+def my_collect(array)
+  if block_given?
+    counter = 0
+    new_collection = []
+    while counter < array.length
+      indexed_element = yield(array[counter])
+      new_collection << indexed_element
+      counter += 1
+    end
+    new_collection
+  else
+    puts "Hey, no block was given!"
+  end
+end
