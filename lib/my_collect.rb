5ce8b552d27dfd8ec9bb5fbbfd9618d399ddb601
bmolina-nyc <bruce.d.molina@gmail.com>
2016-05-15 20:42:16 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,9 @@
-
-def my_each(array)
-  i = 0 
-  while i < array.length 
-    yield(array[i])
-    i += 1 
-  end
-  array 
-end
-
 def my_collect(array)
-  result = [] 
-  my_each(array) do |el|
-    result << el.upcase
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
   end
-  result 
+  collect
 end
-
-
