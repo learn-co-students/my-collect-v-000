feba677a38ddb41e859f9e17ce4d353a17e30c57
Dom McKellar <dom.mckellar@gmail.com>
2016-05-20 11:45:25 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+languages = ["Tim", "Tom", "Jim"]
+def my_collect(array)
+  count = 0
+  collection = []
+  while count < array.length
+    collection << yield(array[count])
+    count += 1
+  end
+  collection
+end
 
-
+my_collect(languages) {|name| name }
