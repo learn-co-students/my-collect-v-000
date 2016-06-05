c39ed8b8286a8b024b357d2b8826955dd601f105
jack woodward <jackwoodwardschool@gmail.com>
2016-06-04 17:50:13 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])  #pushes return value of yield into array collection
+    i += 1
+  end
+  collection  # returns collection array at end of method hello
+end
