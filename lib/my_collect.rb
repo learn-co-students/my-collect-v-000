38207febd0ecb682b7b11e5144a7abc9657df3e9
scottibe <bewick.scott@gmail.com>
2016-06-09 22:37:36 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+require 'pry'
+def my_collect(collection)
+  i = 0
+  stu_names = []
+  while i < collection.length
+    stu_names << yield(collection[i])
+    i += 1
+  end
+  stu_names  
+end
 
