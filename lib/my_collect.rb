294fcb7b6c0363a4b7ad20d9724ce8d98a571f11
David Ward <808dward@gmail.com>
2016-06-18 04:17:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,9 @@
 def my_collect(array)
-	i = 0
-	collect = []
-	while i < array.length
-		collection << yield(array[i])
-		i += 1
-	end
-	collect
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
-
-# do |name|
-#  name.split(" ").first
-#end
