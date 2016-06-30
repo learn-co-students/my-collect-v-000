3538c3f71a271701c5e7a7b8d263cdb0c33ac812
Binny Lewis <binnylewis@gmail.com>
2016-06-30 10:11:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  student = []
+  while i < array.length
+    student << yield(array[i])
+    i += 1
+  end
+  student
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |student|
+  student.split(" ").first
+end
