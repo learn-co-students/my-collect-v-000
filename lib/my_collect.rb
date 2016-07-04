8326c3bc7d105016d82fc82ac12ed20759fa3763
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-04 00:13:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0
+  return_collection = []
+  while i < collection.length
+    return_collection << yield(collection[i])
+    i += 1
+  end
+  return_collection
+end
