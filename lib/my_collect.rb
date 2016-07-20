edb0ed0737dfd0763e110448ad781df690ac9c7b
Juan Gongora <gongora.animations@gmail.com>
2016-07-20 00:33:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(name)
+    counter = 0
+    collection = []
+    while counter < name.size
+        collection << yield(name[counter])
+        counter += 1
+    end
+    collection
+end
