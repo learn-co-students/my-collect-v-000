df26c8d16753c7bd28a89f14262eec59f4ee69d7
AlicT <alic.trossman@gmail.com>
2016-08-03 15:58:47 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
+  collection_with_uppercase = []
+  while i < collection.length
+    collection_with_uppercase << yield(collection[i])
+    i += 1
+  end 
+  collection_with_uppercase
+end
+
+ 
 
 
