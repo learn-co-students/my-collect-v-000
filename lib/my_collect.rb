448b1a4923f0f83225b1244ae60901cb1370b784
Justin  Gore <justin.gore@outlook.com>
2016-08-26 23:51:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  collection_languages = []
+  counter = 0
+  while counter < array.length
+    collection_languages << yield(array[counter])
+    counter+=1
+  end
+collection_languages
+end
