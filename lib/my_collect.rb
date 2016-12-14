5d0c7762d0778dac5c768636c96a05880e4bc382
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2016-12-14 21:12:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+  collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
