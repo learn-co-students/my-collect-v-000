c798d72222c33e3e88ec0cf09679e5e9a297c4ed
charlie lee <ctlee9096@gmail.com>
2016-12-18 23:30:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
 
+def my_collect(array)
+  i = 0
+  lempty_array = []
+  while i < array.length
+    #binding.pry
+      lempty_array << yield(array[i])
+    i += 1
+  end
+  lempty_array
+end
 
+#binding.pry
+#hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
