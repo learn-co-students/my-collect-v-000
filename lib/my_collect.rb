acccfeb956df13177afad7e1f0a0a8d202bcd24d
Damien de Lartigue <damien@delartigue.co.uk>
2017-01-26 17:44:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
 
+def my_collect(arr)
+  i = 0
+  new_arr = []
+  while i < arr.length
+    new_arr << yield(arr[i])
+    i += 1
+  end
+  new_arr
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
