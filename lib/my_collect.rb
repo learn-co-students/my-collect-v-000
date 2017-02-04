6ec46854f33040fe4bad543536d7a5dbd7d06f69
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-04 17:31:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  count = 0
+  collection = []
+  while count < array.length
+    collection << yield(array[count])
+    count += 1
+  end
+  collection
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) { |name| name.split(" ").first }
 
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+
+#hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
