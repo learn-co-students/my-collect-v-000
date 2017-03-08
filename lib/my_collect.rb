5dc36e647180afa6bfe8b63f1ff0fce8c6a21a28
Rachel Walwood <walwoodr@gmail.com>
2017-03-08 22:53:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# require 'pry'
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.size
+    collection << yield(array[i]) if block_given?
+    i += 1
+  end
+  collection
+end
 
+# binding.pry
