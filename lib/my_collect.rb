a42b42f094dba11abe838dcd73df5413f0efa941
Daniela Carvajal <d.carvajal.agama@gmail.com>
2017-03-09 16:25:08 +0000

    done my collect lab

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  i = 0
+  new_collection = []
+  while i < array.length
+    new_collection << yield(array[i])
+    i = i + 1
+  end
+  new_collection
+end
 
+#my_collect(collection) do |name|
+#  name.split(" ").first
+#end
 
+#my_collect(collection) do |lang|
+#  lang.uppercase
+#end
