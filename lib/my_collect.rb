0d28d5d5acecab038aa49d7fa6c9ea2b69896e11
Anthony McCormick-Gonzales <a.j.mccormickgonzales@gmail.com>
2017-03-11 01:47:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+#my_collect(collection) do |name|
+#  name.split(" ").first
+#end
 
+#my_collect(collection) do |lang|
+#  lang.uppercase
+#end
 
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i = i + 1
+  end
+  new_collection
+end
