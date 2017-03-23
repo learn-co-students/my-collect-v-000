37943bbbb50f31edb5b70d26dfe1b9b268da0e4c
Zenia Villa <zeniavilla@hotmail.com>
2017-03-23 10:46:41 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+    i = 0
+    new_collection = []
+    while i < collection.length
+       new_collection << yield(collection[i]) if block_given?
+       i += 1
+    end
+    new_collection
+end
 
-
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+#    name.split(" ").first
+#end
