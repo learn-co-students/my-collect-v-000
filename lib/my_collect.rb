7b5e721fe35c7b377a499dcefe6c5d655d40fcf4
Harleigh Abel <harleighabel@gmail.com>
2017-03-23 04:17:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+languages =['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
 
+def my_collect(array)
+  counter = 0
+  new_array = []
+  if array.empty?
+    return
+  else
+    while counter < array.length
+    new_array << yield(array[counter])
+      counter += 1
+    end
+  new_array
+  end
+end
+
+my_collect(students) do |student|
+  student.split(" ")
+end
+
+my_collect(languages) do |language|
+  language.upcase
+end
