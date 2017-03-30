116a1f2887240983122ada7ddd3c4666787205a3
Aditi Green <asaki444@gmail.com>
2017-03-30 21:30:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+second_array = []
+i = 0
+while i < array.length
+    if array[i].split(' ').length == 2
+    second_array << yield(array[i].split(" ").first)
+    else
+      second_array << yield(array[i].capitalize)
+      end
+     i+=1
+   end
+second_array
+end
