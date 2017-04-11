9a131d44da0e7a3bee65f8d877c928e5e59471ca
Julia Thompson <thompson.julia@gmail.com>
2017-04-11 11:15:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(original_collection)
+  new_collection = []
+  i = 0
+  if block_given?
+    while i < original_collection.length
+      new_collection << yield(original_collection[i])
+      i += 1
+    end
+    new_collection
+  end
+end
