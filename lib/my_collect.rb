622b6e92171d26a77ec409ca24cb45a2bee63f71
Will Sauer <will@heueso.com>
2017-04-19 18:43:06 +0000

    finished

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect (array)
+#housekeeping
+modified_array = []
+array.compact
+if block_given?
 
 
+    counter = 0
+    while counter < array.count
+    modified_array << yield(array[counter])
+    counter += 1
+    end
+
+#return modified array
+modified_array
+
+
+end
+end
