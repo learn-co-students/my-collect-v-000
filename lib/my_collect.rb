89d5acdbb84f74810c7d10b1664f7263b5e4705e
Menachem Hornbacher <me@mendel.tech>
2017-05-18 17:48:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  i = 0
+  res  = []  # we will store the results from the yeilded block here
+  while i < array.count
+    res << yield(array[i])
+    i += 1
+  end
+  res
+end
