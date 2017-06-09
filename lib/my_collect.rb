20c3763bc562863ed009da6d0dfc3b8e0755e165
jamiegiuliano <jamiepgiuliano@gmail.com>
2017-06-09 20:49:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(array)
+    if block_given?
+        i = 0 
+        collection = []
+        while i < array.length
+            collection << yield(array[i])
+            i += 1 
+        end
+        
+        collection 
+        
+        else 
+            puts "No block given!"
+    end
+end
