2af47a898a35f2f2823aca49935fdfd4964b25c5
github username <github email address>
2017-06-14 12:39:06 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+languages = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
 
+    collection << yield(array[i])
+    i += 1
+  end
+    collection
+end
+
+my_collect(languages) do |lang|
+  puts lang.upcase
+end
+
+my_collect(students) do |name|
+  puts name.split(" ").first
+end
