feb29bfe53d889fe8e0b47e43f41ebeb96d8717a
Shangni Hu <shangnihu@gmail.com>
2017-06-14 16:46:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(collection)#!/usr/bin/env ruby -wKU
+  i = 0
+  collect = []
 
-
+  while i < collection.length
+    collect << yield(collection[i])
+    i += 1
+  end
+  return collect
+end
