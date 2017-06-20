8c33099f3c891fdeed624b120fff87c1aa5ba5ae
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2017-06-20 01:21:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(collection)
+  i = 0
+  array = []
+  while i < collection.length
+    array << yield(collection[i])
+#   #     collection
+    i +=1
+  end
+      array
+#   # end
+#   # yield(students)
+#   # #
+# end
+# def my_collect(languages) do |language|
+#    language.upcase
+end
 
-
+# my_collect(students) do |name| name.split(" ").first
+# end
+# array
