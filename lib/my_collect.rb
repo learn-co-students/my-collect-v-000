909b9acc01671af1f09f12e98fc764267f07d7bd
Eric Strickler <eric.strickler@gmail.com>
2017-06-22 20:47:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+require 'pry'
 
-
+def my_collect(collection)
+  i = 0
+  new_collect =[]
+  while i < collection.length
+    new_collect << yield(collection[i])
+    i += 1
+  end
+  new_collect
+end
