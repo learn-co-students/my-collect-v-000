1a2d6c383487e2b47babe70bde57cd9bd0d777fb
Erica Plofsky <ericaplofsky@gmail.com>
2017-06-24 17:02:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array)
+      x = 0
+      collection = []
+      while x < array.length
+        collection << yield(array[x])
+        x = x + 1
+      end
+collection
+end
 
 
+  # call your method here!
+my_collect(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
