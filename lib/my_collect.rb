15bbfaab0f9723028808ab0e94f7ea62af891af3
Susan Fowler <infopro94@gmail.com>
2017-06-25 23:21:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(collection)
+    i = 0
+    things = [ ]
+    while i < collection.length
+        things << yield(collection[i])
+        i += 1
+    end
+    things
+  end
 
+  # def my_collect(collection)
+  #     i = 0
+  #     while i < collection.length
+  #         collection << yield(collection[i])
+  #     i += 1
+  #     end
+  # end
 
+# def my_collect(collection)
+#     i = 0
+#     while i < collection.length
+#         yield(collection[i])
+#         return collection[i]
+#       i += 1
+#     end
+# end
