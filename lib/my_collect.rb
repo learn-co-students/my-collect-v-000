2f423e863d5cce05e1329818b3f13be83ec5804e
Aron Roitblat <roitblatari@gmail.com>
2017-07-12 20:57:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(students)
+  i = 0
+  collect = []
+  while i < students.size
+     collect << yield(students[i])
+     i += 1
+  end
+  collect
+end
+
+my_collect(students) do |name|
+  name.split(" ").first
+end
