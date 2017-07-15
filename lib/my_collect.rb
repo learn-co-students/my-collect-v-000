a99ff2849d88cf7b28120d575cdab0701f78d466
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-15 20:20:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  c=0
+  result=Array.new
+  while (c<collection.length)
 
-
+    result.push( yield collection[c])
+    c+=1
+  end # put argument(s) here
+  # code here
+  result
+end
