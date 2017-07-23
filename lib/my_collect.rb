77f69edc160c63152ae3a04417f09351fd692d9b
Yehuda Makarov <yehudamakarov@gmail.com>
2017-07-23 17:57:40 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
 
+def my_collect(array)
+	if array.empty?
+		puts "The array is empty"
+	else
+		i = 0
+		collection = []
+		while i < array.length
+			collection << yield(array[i])
+			i += 1
+		end
+		collection
+	end
+end
+
+
