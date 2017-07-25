a6949bbe8a44eb5ccf4ec455ad2fe4ef2fd38b5b
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-07-25 06:11:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(array)
+  i = 0
+  collection = []
 
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
 
+  return collection
+end
+
+words = ["these", "words", "should", "be", "in", "caps"]
+my_collect(words) do |word|
+   word.upcase
+end
