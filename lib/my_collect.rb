05f065d7bf78056c1c9a4a50964083a483481645
Kyle Poretto <kyleporetto@gmail.com>
2017-08-05 19:08:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,17 @@
 def my_collect(array)
-  i = 0
-  while
+  if block_given?
+    i = 0
+    final = []
+    while i < array.length
+        final << yield(array[i])
+        i = i + 1
+    end
+    final
+  else
+    "No block given"
+    end
+end
 
-  end
+my_collect([1,2,3,4]) do |index|
+  index + 1
 end
