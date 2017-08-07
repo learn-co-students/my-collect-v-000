1ecd0ece55d95d8b4d1f258bc8a4ef99b476df96
Ariana Berger <ariana.berger@gmail.com>
2017-08-07 17:36:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(empty_array)
+  i = 0
+  collection = []
+  while i < empty_array.length
+    collection << yield(empty_array[i])
+    i += 1
+  end
+  collection
+end
 
-
+# my_collect(empty_array) {|x| x.uppercase}
