5b5db12aea4e011aebee623307031da4816ad540
Sam Ojling <s.ojling@gmail.com>
2017-08-07 22:21:59 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  counter = 0
+  returned_array = []
+  while counter < array.length
+    returned_array << yield(array[counter])
+    counter += 1
+  end
+  returned_array
+end
