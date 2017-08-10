fba2cc6a85af1bd80a6d14b0875d9b1cf30d42e0
Archana Burra <burra.archana@gmail.com>
2017-08-10 16:59:10 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  new_array = []
+  counter = 0
+  while counter < array.length
+    new_array << yield(array[counter])
+    counter += 1
+  end
+new_array
+end
 
+full_names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(full_names) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+
+my_collect(collection) do |lang|
+  lang.upcase
+end
