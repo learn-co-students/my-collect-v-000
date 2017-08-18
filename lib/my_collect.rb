1ea5956b90fa315e3eaaac97f7b580053f6ba8f1
Sofia Candiani <sofia.candiani123@gmail.com>
2017-08-18 21:56:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  #begin counter at 0 (counter = i)
+  i = 0
+  #start with an empty collection
+  collection = []
+  #while the counter is less than the array length,
+  while i < array.length
+    #collection should be added to the yielded value
+  collection << yield(array[i])
+  #continues incrementation while i < array.length
+  i +=1
+end
+#returns 
+collection
+end
 
-
+## notes ##
+# my_collect should function for various collections
