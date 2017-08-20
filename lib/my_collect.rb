2c18f01f43859d2062fd348e74fc3c7009b0bfb1
Lara Sanders <larasanders@gmail.com>
2017-08-20 17:19:18 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
+def my_collect(collection)
+i = 0
+collect_reverse = []
+while i < collection.length
+  collect_reverse << yield(collection[i])
+i = i + 1
+end
+collect_reverse
+end
 
+my_collect(["Sam", "Joe", "Bob", "Stan"]) do |name|
+  name.reverse
+end
