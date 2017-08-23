ebe2cdb777de197a43ac9421bf9f3caf4bc4067f
Kaitlin Kelly <kk504408@gmail.com>
2017-08-23 16:24:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
+def my_collect (array)
+  if block_given?
+    i = 0
+    collect = []
+    while i < array.length
+      collect << yield(array[i])
+      i = i + 1
+    end
+    collect
+  else
+    return "No block given!"
+  end
+end
 
+names = ["Dan Torre", "Kaitlin Kelly", "John Doe"]
+my_collect (names) do |name|
+  name.split(" ").first
+end
