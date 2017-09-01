05feca89fd39a13005661ec2cf8c2dd3060abdb9
Christine <christinetran825@gmail.com>
2017-09-01 18:59:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
+
+def my_collect(array)
+  collection = []
+  i = 0
+  while i < array.length
+    #binding.pry
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 
+#my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name| name.split(" ").first }
+#my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |lang| lang.upcase }
