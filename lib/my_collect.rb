7d1cccf862e7d18f35ab28611142e82012a1eaa0
aisayo <aisayo@icloud.com>
2017-09-05 22:23:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collections) #method and argument of collections
+  c = 0
+  new_collections = [ ]
+  while c < collections.length
+    new_collections << yield(collections[c])
+    c += 1
+  end
+  new_collections
+end
