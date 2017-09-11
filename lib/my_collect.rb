013628fce405a1d1841bf2e60c85206301348bf0
Amber Blahnik <blissedtodays@gmail.com>
2017-09-11 00:33:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(array)
+  # Set counter to 0
+  counter = 0
+  # Create new, empty array variable
+  collection = []
+  # While counter is less than array length
+  while counter < array.length
+    # Yield the value of the array at counter, shovel into new array
+    collection << yield(array[counter])
+    # Counter incrementing + 1
+    counter += 1
+  end
+  # Return the new array
+  collection
+end
