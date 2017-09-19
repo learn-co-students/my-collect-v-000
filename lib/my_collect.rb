a1bf9b6c8e91a0435221559a7b8360a7603fbff1
Justin Daniel <justinddaniel@gmail.com>
2017-09-19 15:01:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  if block_given?
+    while i < collection.length
+      new_collection << yield(collection[i])
+      i += 1
+    end
+    new_collection
+  else
+    "Hey no block was given!"
+  end
+end
