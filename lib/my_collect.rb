fe55e866f432ae904fab72e30b6362848ebefc72
Frank McCallion <keith@sensortech.com>
2017-09-23 20:10:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+languages = []
 
+def my_collect(collection)
 
+  i = 0
+  lang = []
+  while i < collection.length
+    lang << yield(collection[i])
+    i += 1
+  end
+  return lang
+end
+
+my_collect(languages) { |language| name.upcase }
