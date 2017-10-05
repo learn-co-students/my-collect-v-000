314c2f875691a23f848643533ac784e69c57acb8
Araba Taylor <araba.taylor20@gmail.com>
2017-10-05 22:55:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(empty_array)
+    i = 0
+    new_array = []
+      while i < empty_array.length
+        new_array<<yield(empty_array[i])
+        i += 1
+      end
+    new_array
+end
