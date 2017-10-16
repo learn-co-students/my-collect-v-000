5f46a4aef0141dd356a69921464e80dfe42ba915
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-16 18:52:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    return collection
+  else
+    "No block given."
+  end
+end
