9b4c7272a7dee3f055835db4f0bb03006620859f
Christopher Harvey <chharvey@icloud.com>
2017-10-21 23:51:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(items)
   i = 0
   collection = []
   while i < items.length
-    collection << yield(items[i]) #yield operates on each item in the array, not the array
-    i = i + 1
+    collection << yield(items[i])
+    i += 1
   end
   collection
 end
