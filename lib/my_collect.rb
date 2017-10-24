d4d8b2449f7782c7b38499b1bfc7bc9b2caccd0e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-24 18:34:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(arr)
+  puts block_given?
+  if block_given?
+    collection = []
+    i=0
+    while i < arr.size do
+      collection << yield(arr[i])
+      i += 1
+    end
+    #puts collection.inspect
+    collection
+  else
+    puts "Nil"
+    NIL
+  end
+end
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+puts my_collect(array) {|name| name.split(" ").first}.inspect
 
