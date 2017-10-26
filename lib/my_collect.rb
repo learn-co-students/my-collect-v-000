c5c3c278afc742e253074de11a33d0397ef5f36e
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-26 03:02:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
-
+def my_collect(array)
+  if block_given?
+    c = []
+    i = 0
+    while i < array.length
+    c <<  yield(array[i])
+      i += 1
+    end
+    c
+  else puts "Missing Block"
+  end
+end
