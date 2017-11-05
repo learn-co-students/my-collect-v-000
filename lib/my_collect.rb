7f90f1751de8ab6f1156d1b1146a58f8c565f8b9
Yina Zhu <yinagit@gmail.com>
2017-11-05 22:43:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,3 +4,11 @@ students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(students) do |name|
   name.split(" ").first
 end
+
+What if your method was being invoked with a totally different collection and a totally different code block? For example, let's say your #my_collect method is given an argument of a list of programming languages and passed a block that capitalizes the elements yielded to it.
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+
+# => ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
