082346b5a8623a85aa04f0279c6bd71be86a8540
Neena van de Water <nkuriakose@hotmail.com>
2017-11-09 21:42:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+require 'pry'
 
-
+def my_collect(collection)
+  i = 0
+  completed_names = [ ]
+  while i < collection.length
+    completed_names << yield(collection[i])
+    i += 1
+  end
+  completed_names
+end
