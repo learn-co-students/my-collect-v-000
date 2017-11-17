e170514ea21111530de99988907eddd0ace87190
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-11-17 02:03:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array) #Created the collect iterator with a while loop again
+  collection = [] #Create a new array we'll use to return at the end of the method
+  i = 0 #the counter will add onto this number
+  while i < array.length
+    collection << yield(array[i]) #we add the elements of the array into the new array we'll return at the end of the method
+    i += 1 #Adds to the counter
+  end
+  collection #returns new array
+end
