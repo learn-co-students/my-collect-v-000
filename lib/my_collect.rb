14145fd0b804ea28d198a4caed311b0eb9bb8f80
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-23 03:04:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if array == []
+    puts "This array is empty"
+  else
+    i = 0
+    new_array = []
+    while i < array.length do
+      new_array << yield(array[i])
+      i += 1
+    end
+  end
+  new_array
+end
