d2a2d5838d5615be637c66ed37d61250529bfac6
Annette <micha232@umn.edu>
2017-11-28 02:15:09 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
 
+def my_collect(collection)
+    cap_collection = []
+    if block_given?
+        i = 0
+       while i < collection.size
+        cap_collection << yield(collection[i])
+        i += 1
+       end
+    end
+    cap_collection
+end
