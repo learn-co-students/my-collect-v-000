d4c45d95b135229a1a7cd57ae40586783923d0dc
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-01 15:53:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
 
+def my_collect(empty_array)
+  collection = []
+  item = 0
 
+  while item < empty_array.length
+    collection << yield(empty_array[item])
+    item += 1
+  end
+    collection
+end
