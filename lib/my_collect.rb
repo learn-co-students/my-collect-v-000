226f0d3394e225d5a8d68d2664cbbbfbaa60b76b
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-05 18:37:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+require "pry"
+def my_collect(array)
+  counter = 0
+  languages = []
+  while counter < array.length
+    languages << yield(array[counter])
+    counter += 1
+  end
+  languages
 
-
+end
