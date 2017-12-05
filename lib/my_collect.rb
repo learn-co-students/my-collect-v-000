c5279b29b4e8789323d097726a3a0e11d578504f
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-05 00:37:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,33 @@
+require 'pry'
+# def my_collect(collection)
+#   collection.each do |lang|
+#   lang.upcase
+#   end
+#   collection
+# end
 
+# def my_collect(collection)
+#   newarray = []
+#   counter = 0
+#   my_collect(empty_array) do |x|
+#     counter += 1
+#   end
+# collection
+# end
 
+def my_collect(collection)
+  newarray = []
+  i = 0
+  while i < collection.length
+  newarray << yield(collection[i]) 
+    i = i + 1
+  end
+  newarray
+end
+
+# def my_collect(collection)
+#   array.each do |name|
+#   name.split(" ").first
+#   end
+#   collection
+# end
