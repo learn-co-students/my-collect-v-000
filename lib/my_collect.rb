9bb1115ef1f956efa555885a9239142bafc0d454
Jordan Kline <jordan.kline@gmail.com>
2018-01-05 17:42:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
-
+def my_collect(array)
+  counter = 0
+  return_array = []
+  if array.length == 0
+    return 0
+  else
+    while counter < array.length
+      return_array << yield(array[counter])
+      counter += 1
+    end
+  end
+  return_array
+end
