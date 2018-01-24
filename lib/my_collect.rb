5cb016e178333673d0cfb95d00acd6352695cd7b
Brennan Heisler <learn@heislercreative.com>
2018-01-24 04:02:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-
+# names = []
+# my_collect(names) do |name|
+#   name.split(" ").first
+# end
+#
+# languages = []
+# my_collect(languages) do |language|
+#   language.upcase
+# end
