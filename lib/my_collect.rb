5444142f4f748912581d92e1a11775daf0b0808c
Kim Gonzales <kimgo589@gmail.com>
2018-01-25 19:14:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    x = yield collection[i]
+    new_collection << x
+    i +=1
+  end
+  new_collection
+end
