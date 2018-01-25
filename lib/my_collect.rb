7b4895035b52dcc45b9bc013fe0eae44ef208074
Eden Mesfun <emesfun@gmail.com>
2018-01-25 16:19:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+def my_collect(languages)
 
-
+  counter = 0
+  collection = []
+  while counter < languages.length
+    collection << yield(languages[counter])
+    counter += 1
+  end #while
+  collection
+end #method
