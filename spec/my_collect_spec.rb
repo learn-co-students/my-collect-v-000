3fd61edeafc65908dafab3996ce79b45f1e6f508
Brandon Brasson <rocksong622@gmail.com>
2018-02-02 20:19:02 +0000

    Done.

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -1,4 +1,4 @@
-describe "my_collect" do
+puts describe "my_collect" do
   let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
   let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
 
