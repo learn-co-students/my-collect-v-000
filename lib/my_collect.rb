812af42c8aea6839a94ae4219bd2ff8049a9d082
Zac Willmington <zac.willmington@gmail.com>
2018-02-07 01:49:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(languages)
+  if languages.length == 0
+
+    puts "no items in array"
+  else
+  languages.collect do |language|
+    yield(language)
+    
+  end
+end
+end
 
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |i| i.upcase}
