df4daf73643f6f2393c356fed1325c5bb547c1de
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 02:43:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  i = 0
+  collection_array = []
+  while i < collection.length
+    collection_array << yield(collection[i])
+    i += 1
+  end
+  collection_array
+end
 
