57408d4447cee4e0d9bbeb2a5e244a5d834d8e1b
Kristin Reddington <kristinreddington@gmail.com>
2018-02-28 20:22:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+food_item = 0
+adaptogens_list = []
+    while food_item < collection.length
+  adaptogens_list << yield(collection[food_item])
+  food_item += 1
+end
+adaptogens_list
+end
