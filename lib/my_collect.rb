3b4fdffc2ca4b1d9ef91cea10d5fb447e93b197d
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 21:22:44 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)  #some code block to be executed at call time
+  if block_given?
+    i = 0
+    x = []
+    while i < array.length
+      y = yield(array[i])
+      x << y
+      i += 1
+    end
+    x
+  else
+    "You didn't provide a code block"
+  end
+end
