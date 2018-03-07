9208e418d47f9cabfbf43d8dc589c05e995b8b4e
Andrew Kairys <andrewkairys@gmail.com>
2018-03-07 17:51:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while array.length > i
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
-
+cats = ["ian", "nugget", "baby"]
+my_collect(cats) {|i| puts i}
