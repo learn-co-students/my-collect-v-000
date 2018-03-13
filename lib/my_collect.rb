882a2f6751f8f0cc63e29075155e970e153c8306
Emily Welsch <emwelsch@gmail.com>
2018-03-13 17:23:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
   i = 0
+  array = []
   while i < collection.length
-    yield(collection[i])
+    array << yield(collection[i])
     i = i + 1
   end
-  collection
-  # collection.upcase
+  array
 end
 
 # call methods
