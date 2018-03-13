535b9d169a9b3508e9fd9eec13aee0e1e52eb5a9
Emily Welsch <emwelsch@gmail.com>
2018-03-13 18:06:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(collection)
-  i = 0
-  array = []
-  while i < collection.length
-    array << yield(collection[i])
-    i = i + 1
+  i = 0 # start iterating at the first element in the array
+  array = [] # identify a new array
+  while i < collection.length # start a loop that goes thru all elements in the array
+    array << yield(collection[i]) # refer now to the code you call in a method
+    i = i + 1 # advance to the next element in the array
   end
-  array
+  array # return the new array. it will contain the updated elements based on what the method executed
 end
 
 # call methods
