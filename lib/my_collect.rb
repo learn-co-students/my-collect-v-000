5bdc4927f7dce9577c5edd873bc13d216cca8cd0
Emily Welsch <emwelsch@gmail.com>
2018-03-13 16:47:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+  # collection.upcase
+end
 
-
+# call methods
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
+#
+# my_collect(collection) do |name|
+#   name.split(" ").first
+# end
