28bc296bb077cc2071d46bc7f00c402c22b18baf
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-14 03:42:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect (languages)
+    updated_array = []
+    i =0
+    while i < languages.length
+      updated_array << yield(languages[i])
+      i += 1
+    end
+    updated_array
+end
