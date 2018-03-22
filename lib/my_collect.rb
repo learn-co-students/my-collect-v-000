ecd7765f094d0391cd3465436ad57aa378a4e40f
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-22 11:49:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    yield(array[i])
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+array = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(array) do |lang|
+  lang.upcase
+end
