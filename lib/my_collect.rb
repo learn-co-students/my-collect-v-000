65fdfc32b616575de97544773c2cda0f778ee307
Diogo Ide <diogoide@gmail.com>
2018-04-04 23:28:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def  my_collect(array)
+  if block_given?
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+        i += 1
+    end
+    return collection
+  else
+    false
+  end
+end
+    #return modified collection
+students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+
+my_collect(students) do |name|
+    name.split(" ").first
+end
 
 
+languages = ["ruby,", "javascript", "python", "objective-c"]
+
+my_collect(languages) do |lang|
+   lang.upcase
+end
