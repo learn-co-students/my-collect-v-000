3759f115f5f24a870d870122e8dd9eb46bc75151
Amanda M <amandaseagull3@gmail.com>
2018-04-06 18:37:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+# def hello(array)
+#   i = 0 
+#   collection = []
+#   while i < array.length
+#   collection << 
+#     yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
 
 
+
+def my_collect(empty_array)
+  i = 0
+  collection = []
+  while i < empty_array.length
+  collection <<
+    yield(empty_array[i])
+    i += 1
+  end
+  collection
+end
