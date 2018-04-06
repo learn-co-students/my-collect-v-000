29fe2886284186eda977a81a31ffc5a5c32a35e3
Isaac Villicana <villicana.i@gmail.com>
2018-04-06 19:13:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if array.empty?
+      "Collection is empty!"
+  else
+    i = 0
+    collection = Array.new
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    collection
+  end
+end
