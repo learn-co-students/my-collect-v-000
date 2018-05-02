47b699540318335bcaf7045f406a93a90fa193fa
Eric Chrobak <eric@ericchrobak.com>
2018-05-02 00:47:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(placeholder)
+  i = 0
+  return_value = []
+  while i < placeholder.length
+    return_value << yield(placeholder[i])
+    i += 1
+  end
+  return_value
+end
 
-
+array = []
+my_collect(array) do |name|
+  name.split(" ").first
+end
+collection = []
+my_collect(collection) do |lang|
+  lang.upcase
+end
