fbac4ff9b9bd0ea43c2b8edeaef45574ff44f275
Dmitri Mendis <2dmendis@gmail.com>
2018-05-03 23:41:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+def my_collect(languages)
+  i = 0 
+  first = []
+  second = []
+  while i < languages.length 
+  
+  yield(languages[i])
+  if languages[i].scan(/\w+(\p{ASCII}\w)?/).size == 1
+  first << languages[i].upcase
+else 
+  first << languages[i].split(" ").first
+end 
+  #second << languages[i].split(" ").first
+  i += 1
+end 
+first 
+#second
+end 
 
