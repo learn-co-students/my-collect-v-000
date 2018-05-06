69ae44b268111eebbe48d1b4063b349b9d99e296
Dale Terrell <tragicid@gmail.com>
2018-05-06 17:55:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,22 @@
-def collection
- collection = ['ruby', 'javascript', 'python', 'objective-c']
-  yield my_collect(collection) do |lang|
-  lang.upcase
-  yield 
-  my_collect
-end
-end
+  # def collectiion
+  # collection = ['ruby', 'javascript', 'python', 'objective-c']
+  #     yield my_collect(collection) do |lang|
+  #     lang.upcase
+  #     yield 
+  #     my_collect
+  #   end
+  # end
+  
+  def my_collection(collection)
+    if block_given?
+      i = 0
+      whiled i < array.length
+      yield(array[i])
+      i = 1 + i
+    end
+    array
+  else
+    puts
+  end
+  
+  
