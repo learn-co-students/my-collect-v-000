569e2f958e69ccbb1d623a3f11fe4b0ca9f3cecf
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 01:23:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  if block_given?
+    collect = []
+    i = 0
+    while i < array.length
+      collect << yield(array[i])
+      i += 1
+    end
+    collect
+  end
+end
