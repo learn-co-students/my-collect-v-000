8bc53ce069c2329c81fdbdc5a195bdc3b0f84a48
J Daniel <jdaniel1008@gmail.com>
2018-05-29 05:47:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
+require 'pry'
+def my_collect(collection)
+  i = 0
+  my_collection = []
+  while i < collection.length
+    my_collection << yield(collection[i])
+    i += 1
+  end
+  my_collection
+end
 
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
