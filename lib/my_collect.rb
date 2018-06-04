fe5f5c9a714024294eaaf896a24f379fef786971
Jed Peek <jedpeek@yahoo.com>
2018-06-04 22:39:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(arg)
+  i = 0
+  collection = []
+  while i < arg.size
+    collection << yield(arg[i])
+    i += 1
+  end
+  collection
+end
 
-
+my_collect(["Banana", "Apple", "Fruit"]){|x| puts x.upcase}
