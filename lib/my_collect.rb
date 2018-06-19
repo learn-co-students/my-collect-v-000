392092ad8e4d3700a8719a00c30fb6f2b4dc6f13
Edsel Caprice <caprice@mail.usf.edu>
2018-06-19 23:39:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
 
+def my_collect(array)
+i = 0
+new_array = []
+if array.length != 0
+while i < array.length
+  new_array << yield(array[i])
+  i += 1
+end
+else
+new_array << array
+end
+new_array
+end
