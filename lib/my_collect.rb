0d2c94a279183b891ac9b8fecb5e2fb9dc09fc18
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 16:20:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
 
+  i = 0
+  collection_mapped = []
+  while i < collection.length
+    collection_mapped << yield(collection[i])
+    i += 1
+  end
 
+  collection_mapped
+end
