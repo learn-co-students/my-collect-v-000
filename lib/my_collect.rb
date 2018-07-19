34e16e862b287f71639c189da0b2283869c881e2
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-19 03:11:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(empty_array)
+  i = 0 
+  collections = [] 
+  
+ while i < empty_array.length
+    collections << yield(empty_array[i])
+    i += 1
+  
+  
+end
+collections
+end 
 
 
+
+
+  
