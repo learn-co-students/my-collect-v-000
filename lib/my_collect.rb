7153647ebcaaeec6d6c532d44b3b2e8a7581eadf
Anita Beauchamp <amadamspr@gmail.com>
2018-07-22 19:01:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+def my_collect(array)
+  #Create counter
+  i = 0
 
+  #Create collection array to hold values
+  collection = []
+
+  #loop through array and yield each element
+  while i < array.size
+    collection << yield(array[i])
+    i += 1
+  end
+
+  #return collection array
+  collection
+end
