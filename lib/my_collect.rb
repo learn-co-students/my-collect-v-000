52c25e99ef9caaf8bf8e982b670e8782aa52abb3
Joseph Senders <jasenders@gmail.com>
2018-08-08 14:16:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  collection = []
+  i = 0
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
