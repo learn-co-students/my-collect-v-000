3152f0190c9a0dce8553883772f6ab33d3ef23d5
Steven Crouse <sdcrouse91@gmail.com>
2018-08-13 08:14:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+  new_collection = []
+  x = 0 
+  
+  while x < collection.size 
+    new_collection << yield(collection[x])
+    x += 1 # Remember to include this to avoid making an infinite loop!
+  end
+  
+  new_collection
+end
 
