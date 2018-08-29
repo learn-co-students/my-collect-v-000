13a997160360ba94df1273b83440e947e9c2a9cf
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-29 03:46:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
 
+def my_collect(array)
+  modified_array = Array.new
+  i = 0
+  while i < array.size
+    modified_array.push(yield array[i])
+    i += 1
+  end
+
+  modified_array
+
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
