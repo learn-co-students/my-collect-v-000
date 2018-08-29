456078098448656a6cc7d755935b9c7fff811a44
David Zeit <dzeit216@gmail.com>
2018-08-29 20:24:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(array)
+  element = 0
+  new_array = []
+  while element < array.length
+  new_array << yield(array[element]) # yield not only pauses but also passes the formula block to modify each element.
+  element += 1
+  end
+  new_array
+end
