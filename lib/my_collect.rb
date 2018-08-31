f0ea17ff82c925c7addd7eb1a71870f292b301a1
Ashley Wong <ashwong04@gmail.com>
2018-08-31 02:40:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(arr)
+  
+  
+  
+  
+
+end
 
 
+
+
+
+# take an argument of a collection
+# iterate over collection using while loop
+# execute code in block with each element in collection using yield
+# should returned modified collection
