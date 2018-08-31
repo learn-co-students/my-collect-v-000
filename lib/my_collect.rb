35a1105e9f5eafa672798c59b60ab5dee8fe9278
Ashley Wong <ashwong04@gmail.com>
2018-08-31 13:25:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,19 @@
-def my_collect(lang, names)
+def my_collect(lang)
   i = 0
   new_lang = []
   while i < lang.length  
-    new_lang.push(yield lang[i].upcase)
+    new_lang.push(yield lang[i])
     i += 1
   end
   new_lang
-  
-  i = 0
-  new_names = []
-  while i < names.length
-    new_names.push(yield names[i].split(" ").first)
-    i += 1
-  end
-  new_names
-  
 end
 
 
+
+  # i = 0
+  # new_names = []
+  # while i < names.length
+  #   new_names.push(yield names[i].split(" ").first)
+  #   i += 1
+  # end
+  # new_names
