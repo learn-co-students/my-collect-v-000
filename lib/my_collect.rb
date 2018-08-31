4919c9f5b070055a19e904e38eb4e0ddd87a5fcb
Ashley Wong <ashwong04@gmail.com>
2018-08-31 12:39:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,26 +1,20 @@
-def my_collect(lang, students)
+def my_collect(lang, names)
   i = 0
   new_lang = []
-  
   while i < lang.length  
     new_lang.push(yield lang[i].upcase)
     i += 1
   end
-  
-  j = 0
-  students_first = []
-  while j < students.length
-    
-  
-  
   new_lang
+  
+  i = 0
+  new_names = []
+  while i < names.length
+    new_names.push(yield names[i].split(" ").first)
+    i += 1
+  end
+  new_names
+  
 end
 
 
-
-
-
-# take an argument of a collection
-# iterate over collection using while loop
-# execute code in block with each element in collection using yield
-# should returned modified collection
