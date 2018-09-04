32133de81a69ff8e7035352ac04e3770424abcc3
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-04 14:30:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
 
+def my_collect(collection)
+ i = 0 
+ collection = []
+ while i < collection.length 
+ collection << yield(collection[i])
+ my_collect(collection) do |name|
+    name.split(" ").first
+  end
+  yield 
+end
+end
