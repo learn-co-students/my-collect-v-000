30eb95c7f20253c275e6287cfbfee672886fc7ab
Derrick Reyes <derrickreyes@me.com>
2018-09-08 01:53:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(grouping)
+  i = 0
+  new_group = [ ]
+  while i < grouping.length
+    yield(grouping[i])
+    new_group << yield(grouping[i])
+    i+=1
+  end
+  new_group
+end
