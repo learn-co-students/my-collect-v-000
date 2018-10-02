dc059c23a7fcb87ed18fe4875048eba269bae8e6
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-02 16:39:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+# my_collect
+#   can handle an empty collection (FAILED - 1)
+#   yields the correct element from a given collection, in this case languages (FAILED - 2)
+#   returns a new collection of appropriately modified elements, in this case capitalized languages (FAILED - 3)
+#   does not modify the original collection (FAILED - 4)
+#   yields the correct element from the given collection, in this case students (FAILED - 5)
+#   returns a new collection of appropriately modified elements, in this case student first names (FAILED - 6)
+#   does not modify the original collection (FAILED - 7)
 
-
+def my_collect(array)
+  i = 0
+  newArray = []
+  while i < array.length
+    newArray << yield(array[i])
+    i+=1
+  end
+ newArray
+end
