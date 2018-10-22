45667e11fb53f4c3f46a56642dc7f910ad1baa7a
Lois Lee <lois.lee@me.com>
2018-10-22 20:23:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
+def my_collect(collection)
+    i = 0
+    new_array = []
+    while i < collection.length
+      new_array << yield(collection[i])
+      i += 1
+    end
+    new_array
 end
