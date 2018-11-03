8d947552d3c5084440d35775cc53dca72282a08c
katejsenger <katejsenger@yahoo.com>
2018-11-03 22:23:22 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(collection)
   array = []
   x = 0
- while x < array.length
-    collection << yield(array[x])
+ while x < collection.length
+    array << yield(collection[x])
     x += 1
   end
   array
