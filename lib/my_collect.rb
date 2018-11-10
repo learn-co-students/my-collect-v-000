a0aa87242ae5e9877016dddf38565bf6c3bdbf88
Kimberly Aviles <01010110.v@gmail.com>
2018-11-10 06:15:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
 
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.count
+    new_array << yield(array[i])
+    i += 1
+  end
+  new_array
+end
 
+
+#I can follow patterns and build this..
+#But I seriously don't know what's going on..
