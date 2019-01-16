bb0675f715200afd5ebed6af6ec0308566990500
daaaniii33 <daaaniii33@gmail.com>
2019-01-16 04:27:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,13 @@
 def my_collect(array)
 i = 0
 collection = []
-
-
 while i < array.length
 collection << yield(array[i])
 i+=1
 end
  collection
 end
+<<<<<<< HEAD
+=======
+
+>>>>>>> 34ea097995d8ec26af258cf6c5f27ec2b950ca26
