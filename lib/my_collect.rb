04da4c274ca9ec00531a6b8e0202e087b1e0d7bf
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-12 04:00:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  #######Try making an empty array here
+  while i < array.length
+    new_array << yield(array[i])
+    #######Push this into the Empty array yield(array[i])
+    i += 1
+  end
+  new_array #######Return the new array
+end
