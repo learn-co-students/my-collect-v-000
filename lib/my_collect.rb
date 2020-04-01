1ec72cd1f32c314a42d98cb2dc8b78788064d0e7
Vicky Theresa Tak <vttak9@gmail.com>
2020-04-01 23:14:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
 def my_collect(array)
-  array_new = []
-  yield
 
-array_new << i
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
-array_new
